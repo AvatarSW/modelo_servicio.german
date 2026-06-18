d8f2d02bbb4edc8fae9b07ec6a49952d8e951039
Germán Ayala <79701865+AvatarSW@users.noreply.github.com>
2026-06-18 19:35:35 +0200

    add getters

--- src/com/pizzeria/Pizza.java
+++ src/com/pizzeria/Pizza.java
@@ -2,13 +2,25 @@ package com.pizzeria;
 
 public class Pizza {
 
-    public String nombre;
-    public String tamano;
-    public double precio;
+    private String nombre;
+    private String tamano;
+    private double precio;
 
     public Pizza(String nombre, String tamano, double precio) {
         this.nombre = nombre;
         this.tamano = tamano;
         this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTamano() {
+        return tamano;
+    }
+
+    public double getPrecio() {
+        return precio;
     }
 }
